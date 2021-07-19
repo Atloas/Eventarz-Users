8d5d1673dbc7484f65d0c8c1fbf8a76140e73169
Atloas <miter010@gmail.com>
2021-07-19 16:41:37 +0200

    Incoming request logging filter and simplified log formats

---
+++ src/main/java/com/agh/EventarzUsers/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,16 @@
+package com.agh.EventarzUsers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        return filter;
+    }
+}
